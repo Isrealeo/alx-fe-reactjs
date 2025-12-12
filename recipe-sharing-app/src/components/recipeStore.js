1247ab80ddf89e17bf9899cfba0947876ce75395
Nwaminogbe Isreal Ohunmaehuni <nwaminogbeisreal@gmail.com>
2025-12-12 07:36:12 +0100

    Rename getFilteredRecipes to getfilteredRecipes

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -46,7 +46,7 @@ export const useRecipeStore = create(
       setIngredientsFilter: (value) => set({ ingredientsFilter: value }),
       setTimeFilter: (value) => set({ timeFilter: value }),
 
-      getFilteredRecipes: () => {
+      getfilteredRecipes: () => {
         const { recipes, searchTerm, ingredientsFilter, timeFilter } = get();
 
         return recipes.filter((r) => {
